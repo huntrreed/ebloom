7e7984e120f17b90161c0ed80e6f33d1a7d85833
Hunter <hunterreed@mac.lan>
2025-01-29 14:09:32 -0700

    no errors in shop

---
+++ src/utils/contentful.ts
@@ -0,0 +1,28 @@
+import { createClient } from 'contentful';
+
+const client = createClient({
+  space: "8jygecfjg6w0",
+  accessToken: "7GVIsy-Ni30iOf3pwLpjBw_fJFCYpT_K5buqed5wL-g",
+});
+
+// Fetch Products for Store Page
+export async function getProducts() {
+    try {
+      const response = await client.getEntries({ content_type: 'product' });
+      return response.items.map((item) => item.fields);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      return [];
+    }
+  }
+
+// Fetch Gallery Items for Gallery Page
+export async function getGalleryItems() {
+    const entries = await client.getEntries({ content_type: 'galleryItem' });
+
+    return entries.items.map(item => ({
+      title: item.fields.title,
+      images: Array.isArray(item.fields.imageurl) ? item.fields.imageurl.map(img => img.fields.file.url) : [],
+      tags: item.fields.tags || [],
+    }));
+}
